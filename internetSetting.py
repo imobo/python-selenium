27f764d575967c0476320738595cea829ad1856e
yuen92 <31352233+imobo@users.noreply.github.com>
2021-01-20 18:21:33 +0800

    Create internetSetting.py

---
+++ internetSetting.py
@@ -0,0 +1,7 @@
+proxies = {
+        'http': '11.111.111.111:8888',
+        'https': '11.111.111.111:8888',
+            }
+headers = {
+        'User-Agent': '"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0',
+        }
